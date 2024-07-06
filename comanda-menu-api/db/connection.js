22b3be3769f6ac7c69270d1070a6573c5a1aa378
JackSSads <fotosjackson2018@hotmail.com>
2024-07-05 23:05:15 -0300

    Remuve backend

--- comanda-menu-api/db/connection.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const envPath = path.resolve(__dirname, '../../', '.env');
-require("dotenv").config({ path: envPath });
-
-const mongoose = require("mongoose");
-
-const connection = mongoose.connect(process.env.MONGO_ATLAS);
-
-module.exports = connection;
--- comanda-menu-api/routes/loginRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const  LoginController = require("../controllers/loginController");
-
-router.post("/", LoginController.login);
-
-module.exports = router;
