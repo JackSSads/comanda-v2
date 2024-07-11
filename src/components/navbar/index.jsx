dff54780707b8727523af4add22f873f622b80a4
JackSSads <fotosjackson2018@hotmail.com>
2024-07-11 14:42:19 -0300

    add connection monitor

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from "react-router-dom";
 
 import { usePage } from "../../contexts";
+import { useConnectionMonitor } from "../../hooks/connectionMonitor"
 
 import { Back, ArrowRight } from "../../libs/icons";
 
 export const Navbar = ({ title, url, isLogout }) => {
+
+    const isOnline = useConnectionMonitor()
 
     const logoutButton = !!isLogout;
 
@@ -23,8 +26,15 @@ export const Navbar = ({ title, url, isLogout }) => {
 
     return (
         <nav className={`fixed top-0 w-full h-16 px-5 flex ${url ? "justify-between" : logout ? "justify-between" : "justify-center"} items-center bg-[#EB8F00] text-slate-100`}>
-            <h2 className="font-bold uppercase text-[18px]">{title}</h2>
-            
+
+            <div>
+                {!isOnline ? (
+                    <h2 className={`transition-all delay-200 uppercase bg-red-600 px-3 py-2 rounded-md font-bold text-white`}>Sem internet</h2>
+                ) : (
+                    <h2 className="transition-all delay-200 font-bold uppercase text-[18px]">{title}</h2>
+                )}
+            </div>
+
             {url ?
                 <button className="px-3 py-2 rounded-md bg-[#1C1D26] hover:bg-[#EB8F00] hover:text-[#1C1D26] border-2 border-transparent hover:border-[#1C1D26] transition-all delay-75"
                     onClick={backOldPage}
