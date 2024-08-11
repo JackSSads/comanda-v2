235174910bfc425037a7be04971d18d72f33685a
JackSSads <fotosjackson2018@hotmail.com>
2024-08-11 15:27:49 -0300

    exportando modal produtos

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Calc } from "./calc";
 export { Navbar } from "./navbar";
 export { Footer } from "./footer";
 export { NewCheck } from "./newCheck";
+export { ModalProduct } from "./modalProduct";
