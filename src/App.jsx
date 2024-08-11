3c44834f8c141bec3c03b5529e389360aa92a92b
JackSSads <fotosjackson2018@hotmail.com>
2024-08-11 16:32:31 -0300

    Removendo PageProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
 
 import { LayoutBase } from "./layouts"
-import { PageProvider, ToggleViewProvider } from "./contexts";
+import { ToggleViewProvider } from "./contexts";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <PageProvider>
-        <ToggleViewProvider>
-          <LayoutBase title="comandas" url="comandas">
-            <AppRoutes />
-          </LayoutBase>
-        </ToggleViewProvider>
-      </PageProvider>
+      <ToggleViewProvider>
+        <LayoutBase title="comandas" url="comandas">
+          <AppRoutes />
+        </LayoutBase>
+      </ToggleViewProvider>
     </BrowserRouter>
   );
 };
