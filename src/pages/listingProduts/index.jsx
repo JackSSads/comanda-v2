d3b505c0c2e503e12522348a8e3bbcee12048415
JackSSads <contatojacksonsouza@hotmail.com>
2024-08-18 17:51:36 -0300

    corrigindo layout

--- src/pages/listingProduts/index.jsx
+++ src/pages/listingProduts/index.jsx
@@ -236,7 +236,7 @@ export const ListingProduts = () => {
                 </div>
 
                 {itensFiltrados.map((item, index) => (
-                    <div key={index} className="border flex justify-between items-center px-3 py-1 w-full rounded-xl shadow-md">
+                    <div key={index} className="flex justify-between items-center px-3 py-1 w-full rounded-xl shadow-md">
 
                         <div className="w-2/3 flex flex-col items-start">
                             <h3 className="text-slate-900 font-bold">{item.nameProduct}</h3>
